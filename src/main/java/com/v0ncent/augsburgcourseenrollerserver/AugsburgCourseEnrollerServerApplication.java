20591546f7836bf723605d6fad5fad66b5951ec6
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-04-24 22:44:53 -0500

    initial

---
+++ src/main/java/com/v0ncent/augsburgcourseenrollerserver/AugsburgCourseEnrollerServerApplication.java
@@ -0,0 +1,13 @@
+package com.v0ncent.augsburgcourseenrollerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugsburgCourseEnrollerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AugsburgCourseEnrollerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/v0ncent/augsburgcourseenrollerserver/AugsburgCourseEnrollerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v0ncent.augsburgcourseenrollerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugsburgCourseEnrollerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
